f44faaa7a0d4bffa81f0e8eb5213881b536a88f7
mr-manson <nikola777jr@gmail.com>
2024-11-18 17:16:32 +0300

    add iTodo & app

---
+++ src/components/App.tsx
@@ -0,0 +1,26 @@
+import React, {useState, useEffect, useRef} from "react";
+
+import {ITodo} from "../types/data";
+
+const App: React.FC = () => {
+	const [value, setValue] = useState("");
+	const [todos, setTodos] = useState<ITodo[]>([]);
+
+	const addTodo = () => {
+		setTodos([...todos, {
+			id: Date.now(),
+			title: value,
+			isCompleted: false,
+		}])
+		setValue("");
+	}
+
+	return <>
+		<div>
+			<input value={value} onChange={e => setValue(e.target.value)} />
+			<button onClick={addTodo}>Add</button>
+		</div>
+	</>;
+}
+
+export {App};
