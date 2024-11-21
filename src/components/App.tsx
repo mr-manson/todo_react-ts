71161a9314f775fe33b190f42019af737814fa58
mr-manson <nikola777jr@gmail.com>
2024-11-21 20:21:17 +0300

    add types directory & app.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,17 +7,19 @@ const App: React.FC = () => {
 	const [todos, setTodos] = useState<ITodo[]>([]);
 
 	const addTodo = () => {
-		setTodos([...todos, {
-			id: Date.now(),
-			title: value,
-			isCompleted: false,
-		}])
-		setValue("");
+		if (value) {
+			setTodos([...todos, {
+				id: Date.now(),
+				title: value,
+				isCompleted: false,
+			}])
+			setValue("");
+		}
 	}
 
 	return <>
 		<div>
-			<input value={value} onChange={e => setValue(e.target.value)} />
+			<input value={value} onChange={e => setValue(e.target.value)}/>
 			<button onClick={addTodo}>Add</button>
 		</div>
 	</>;
